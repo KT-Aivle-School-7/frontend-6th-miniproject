333408f691dd42fc030005b2c42b496d438fb75b
JeongUk00 <jeongwook@gmail.com>
2026-06-24 13:43:22 +0900

    fix : url

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 //  - Vercel    : handled by api/server.js serverless function
 
 // const BASE = '/api'
-const BASE = 'http://3.101.26.25:8080/api'
+const BASE = 'http://18.144.167.89:8080/api'
 
 async function request(url, options = {}) {
   const res = await fetch(url, {
